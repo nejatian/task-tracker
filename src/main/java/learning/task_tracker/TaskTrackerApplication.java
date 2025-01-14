19b38770301c5e8dacc3e6bbc805589ea2b18086
Nejatians <nejatian@outlook.com>
2025-01-14 14:40:57 +0100

    first commit

---
+++ src/main/java/learning/task_tracker/TaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package learning.task_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/task_tracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.task_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
